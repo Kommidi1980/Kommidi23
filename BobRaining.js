519749682402f67954b289a5c0d663fae8ed50d1
Praneeth Reddy <praneethreddy@hotmail.com>
2020-02-17 22:01:14 -0800

    bobraining.js

---
+++ BobRaining.js
@@ -0,0 +1,10 @@
+var name = "Bob";
+var isRaining = true;
+var cash = 100;
+if (isRaining == true){
+    console.log("I need new coat")
+console.log(cash - 50);
+}
+else{
+    console.log("YAY");
+}
